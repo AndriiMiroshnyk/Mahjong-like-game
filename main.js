19d35dc7f2b69abdeab1b6916edc1ed841b02d83
Andrii Miroshnyk <miroshnykandrii@gmail.com>
2022-06-16 19:28:46 +0300

    Added main logic

--- main.js
+++ main.js
@@ -15,42 +15,54 @@ let primeNumArray = [2, 2, 3, 3, 5, 5, 7, 7, 11, 11, 13, 13, 17, 17, 19, 19, 23,
 shuffle(primeNumArray);
 
 let cardValues = [];
+let cardId = [];
 
 const sleepNow = (delay) => new Promise ((resolve) => setTimeout (resolve, delay));
 
-async function displayCard(text) {
+async function checkCard(text) {
     for (let i = 0; i < cardValues.length; i++) {
+        const elId = text.id;
+        cardId.push(elId);
+        console.log(cardId);
         for (let k = i + 1; k < cardValues.length; k++) {
             if (cardValues[i] !== cardValues[k]) {
                 await sleepNow(1000);
-                text.style.display = "none";
+                for (let item = 0; item < cardId.length; item++) {
+                    let el = document.getElementById(cardId[item]);
+                    console.log(el);
+                    if (cardId.includes(elId)) {
+                        el.style.display = "none";
+                    }
+                }
             }
             cardValues = [];
+            cardId = [];
         }
     }
 }
 
-for (let i = 0; i < 30; i++) {
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    div.className = "card";
-    wrapper.appendChild(div);
-    p.className = "text";
-    p.innerHTML = primeNumArray[i];
-    p.style.display = "none";
-    div.appendChild(p);
-    div.addEventListener('click', () => {
-        p.style.display = "block";
-        cardValues.push(div.textContent);
-        console.log(cardValues);
-        displayCard(p);
-    });
+function Game() {
+    for (let i = 0; i < 30; i++) {
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        let btn = document.querySelector(".btn");
+        div.className = "card";
+        wrapper.appendChild(div);
+        p.className = "text";
+        p.id = i;
+        p.innerHTML = primeNumArray[i];
+        div.appendChild(p);
+        btn.addEventListener('click', () => {
+            p.style.display = "none";
+            btn.style.display = "none";
+        });
+        div.addEventListener('click', () => {
+            p.style.display = "block";
+            cardValues.push(div.textContent);
+            console.log(cardValues);
+            checkCard(p);
+        });
+    }
 }
 
-// const cardList = document.querySelectorAll(".card");
-// console.log(cardList);
-
-// for (let cardIndex = 0; cardIndex < cardList.length; cardIndex++) {
-//     let value = cardList[cardIndex].textContent;
-//     console.log(value);
-// }
+Game();
